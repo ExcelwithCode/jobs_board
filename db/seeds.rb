28784dcf6dd11349a8a3f7ed2c5513a96274d2ee
Raymond <rkcudjoe1@gmail.com>
2015-07-23 22:12:52 -0400

    delete seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,27 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Job.create!([
-  {
-  title: "Copywriter",
-  category: "Software",
-  location: "New York, NY",
-  description: "You read copy on the train.",
-  to_apply: "Send an email to Mike."
-  },
-  {
-    title: "UX Designer",
-    category: "Hardware",
-    location: "Dallas, TX",
-    description: "You need the touch of Ives.",
-    to_apply: "Call Jobs at 555 555 5555."
-  },
-  {
-    title: "Data Scientist",
-    category: "Education",
-    location: "Boston, MA",
-    description: "You gotta love kids.",
-    to_apply: "Drop your resume off at our office."
-  }
-  ])
