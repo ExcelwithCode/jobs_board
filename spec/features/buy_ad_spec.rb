301cc6ab05cdb92f73e5bc33a9bbceb4f881dbd2
Raymond <rkcudjoe1@gmail.com>
2015-07-24 20:12:11 -0400

    complete core features of jobs board

--- spec/features/buy_ad_spec.rb
+++ spec/features/buy_ad_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 require 'rails_helper'
 
-feature "process job payment" do
+feature "process payments for ads" do
+  before { @job = FactoryGirl.create(:job) }
+  
+  scenario "user sees the payment form" do
+    visit buy_ad_path(@job.permalink)
+    
+    expect(page).to have_button("PAY AND DISPLAY AD NOW")
+  end
+  
   scenario "payment succeeds"
-  
-  scenario "payment fails"
-  
-  
+  # do
+  #   visit buy_ad_path(@job.permalink)
+    
+  #   fill_in("Your email address", with:"July24@gmail.com")
+  #   fill_in("Card number", with:"4000056655665556")
+  #   fill_in("Exp Month", with: "03")
+  #   fill_in("Exp Year", with: "2019")
+  #   fill_in("CVC Code", with: "768")
+    
+  #   click_button("PAY AND DISPLAY AD NOW")
+    
+  #   expect(page).to have_text(@job.title)
+  # end
 end
-
